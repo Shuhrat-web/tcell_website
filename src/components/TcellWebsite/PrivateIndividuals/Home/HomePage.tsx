116150c0074457512cb37deaca6a9f1fa1cb3ce3
Islamov Jahongir <jahongirislamov2003j@mail.ru>
2024-09-05 11:12:36 +0500

    added new post and query param for fab button

--- src/components/TcellWebsite/PrivateIndividuals/Home/HomePage.tsx
+++ src/components/TcellWebsite/PrivateIndividuals/Home/HomePage.tsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import PrivateIndividualsNewsSection from './components/PrivateIndividualsNewsSection'
-import PrivateIndividualsProductsSection from './components/PrivateIndividualsProductsSectionProps'
-import PrivateIndividualsFaqSection from './components/PrivateIndividualsFaqSection'
-import PrivateIndividualsAppsSection from './components/PrivateIndividualsAppsSection'
-import PrivateIndividualsTarifsSection from './components/PrivateIndividualsTarifsSection'
-import FromLeftAnimationProvider from '@/components/Ui/AnimationProviders/FromLeftAnimationProvider'
-import ImageBanner from '@/components/Ui/Banners/ImageBanner'
+import React from "react";
+import PrivateIndividualsNewsSection from "./components/PrivateIndividualsNewsSection";
+import PrivateIndividualsProductsSection from "./components/PrivateIndividualsProductsSectionProps";
+import PrivateIndividualsFaqSection from "./components/PrivateIndividualsFaqSection";
+import PrivateIndividualsAppsSection from "./components/PrivateIndividualsAppsSection";
+import PrivateIndividualsTarifsSection from "./components/PrivateIndividualsTarifsSection";
+import FromLeftAnimationProvider from "@/components/Ui/AnimationProviders/FromLeftAnimationProvider";
+import ImageBanner from "@/components/Ui/Banners/ImageBanner";
+import Link from "next/link";
 
 const HomePage = () => {
-
   return (
     <FromLeftAnimationProvider>
       <div>
-        <ImageBanner 
-          img="/images/banner_main.webp"
-          alt='privateIndividualsBanner' 
-        />
-        <div className='t__container mt-24'>
+        <Link href="/ru/millionaire">
+          <ImageBanner
+            img="/images/millionaire_banner.webp"
+            alt="privateIndividualsBanner"
+          />
+        </Link>
+        <div className="t__container mt-24">
           <PrivateIndividualsNewsSection />
           <PrivateIndividualsTarifsSection className="my-24" />
-          <PrivateIndividualsProductsSection className='mb-24' />
-          <PrivateIndividualsAppsSection className='mb-24' />
+          <PrivateIndividualsProductsSection className="mb-24" />
+          <PrivateIndividualsAppsSection className="mb-24" />
           <PrivateIndividualsFaqSection />
         </div>
       </div>
     </FromLeftAnimationProvider>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
