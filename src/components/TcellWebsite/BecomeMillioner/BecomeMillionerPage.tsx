0db83f6c32ddeaa61a79f4ee418cc71b9f55fd07
Islamov Jahongir <jahongirislamov2003j@mail.ru>
2024-09-13 18:37:01 +0500

    added new block about chi gap app in millionaire

--- src/components/TcellWebsite/BecomeMillioner/BecomeMillionerPage.tsx
+++ src/components/TcellWebsite/BecomeMillioner/BecomeMillionerPage.tsx
@@ -4,12 +4,14 @@ import BecomeMillionerParticipationSection from "./components/BecomeMillionerPar
 import BecomeMillionerConditionsSection from "./components/BecomeMillionerConditionsSection";
 import BecomeMillionerToSeeSection from "./components/BecomeMillionerToSeeSection";
 import BecomeMillionerPrizeSection from "./components/BecomeMillionerPrizeSection";
+import BecomeMillionerForChiGapSection from "./components/BecomeMillionerForChiGapSection";
 
 const BecomeMillionerPage = () => {
   return (
     <div className="t__container md:px-40 lg:px-56">
       <BecomeMillionerHeadSection />
       <BecomeMillionerParticipationSection />
+      <BecomeMillionerForChiGapSection/>
       <BecomeMillionerConditionsSection />
       {/* <BecomeMillionerToSeeSection /> */}
       <BecomeMillionerPrizeSection />
---
+++ src/components/TcellWebsite/BecomeMillioner/components/BecomeMillionerForChiGapSection.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Title from '@/components/Ui/Title/Title';
+import { useTranslations } from 'next-intl';
+import React from 'react'
+
+const BecomeMillionerForChiGapSection = () => {
+    const t = useTranslations("Millionaire")
+
+  return (
+    <section className="bg-[#F0F0F0] rounded-[20px] p-5 mt-6">
+      <p className="text-[#262626] text-lg font-semibold">
+        {t("chigap.title")}
+      </p>
+      <p className="text-[#262626] text-base font-normal mt-3">
+        {t("chigap.description")}
+      </p>
+      <p className="text-[#262626] text-base font-normal">
+        {t("chigap.forThis")}
+      </p>
+      <p className="text-[#262626] text-base font-normal">
+        {t("chigap.first")}
+      </p>
+      <p className="text-[#262626] text-base font-normal">
+        {t("chigap.second")}
+      </p>
+      <p className="text-[#262626] text-base font-normal">
+        {t("chigap.third")}
+        <span>
+          <a
+            href="https://wa.me/992927777777"
+            className="text-primary underline decoration-1"
+          >
+            {t("chigap.whatsApp")}
+          </a>
+          {t("chigap.check")}
+        </span>
+      </p>
+    </section>
+  );
+}
+
+export default BecomeMillionerForChiGapSection
